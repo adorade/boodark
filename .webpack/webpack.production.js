15c9c182435f94ac95e4763df934068860dfc877
Marian Bolojan <office.adorade@gmail.com>
2023-03-15 02:05:04 +0200

    build(tools): change webpack performance config

--- .webpack/webpack.production.js
+++ .webpack/webpack.production.js
@@ -16,7 +16,7 @@ exports.productionConfig = () => ({
   },
   performance: {
     hints: 'warning',
-    maxAssetSize: 285000,
-    maxEntrypointSize: 570000,
+    maxAssetSize: 310000,
+    maxEntrypointSize: 620000,
   },
 });
